260743d1c3688e1c8707e08d261c44e994e4c284
João Bittencourt <j.v_dias@hotmail.com>
2024-02-01 09:36:34 -0300

    wip

--- run.sh
+++ run.sh
@@ -2,10 +2,9 @@
 
 KERNEL=$(uname -s)
 ARCH=$(arch)
-EXEC=$1
 
 if [ "$#" -ne 1 ]; then
-    echo 'You need pass parameters like: ./run-sh "--version"'
+    echo 'You need pass an argument like: ./run-sh "--version"'
     exit 1
 fi
 
@@ -17,11 +16,11 @@ fi
 if [ $ARCH == "arm64" ]; then
     echo "Running on arm64 architecture."
     chmod +x ./php/php-arm64
-    ./php/php-arm64 $EXEC
+    ./php/php-arm64 $1
 elif [ $ARCH == "x86_64" ]; then
     echo "Running on x86_64 architecture."
     chmod +x ./php/php-x86_64
-    ./php/php-x86_64 $EXEC
+    ./php/php-x86_64 $1
 else
     echo "Unsupported architecture: $ARCH"
     exit 1
